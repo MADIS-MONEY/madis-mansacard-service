11e66cfe9e1b0e2fd79fc1410056a200aaaf2b81
StephaneEhu <niamianstephane@gmail.com>
2024-10-24 06:07:37 +0200

    Initialisation du service

---
+++ src/test/java/com/madisfinance/mansacard_service/MansacardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madisfinance.mansacard_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MansacardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
