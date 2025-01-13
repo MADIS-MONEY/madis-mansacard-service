613f2d66375aff49d88a705be976b80e266387be
StephaneEhu <niamianstephane@gmail.com>
2025-01-13 05:23:36 +0100

    2KPI effectué

--- src/main/java/com/madisfinance/mansacardservice/MansacardServiceApplication.java
+++ src/main/java/com/madisfinance/mansacardservice/MansacardServiceApplication.java
@@ -2,6 +2,7 @@ package com.madisfinance.mansacardservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -10,6 +11,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 @OpenAPIDefinition(info = @Info(title = "Madis Mansa Card Operations API", version = "1.0", description = "Madis Card : User Card Management"))
 public class MansacardServiceApplication {
 
